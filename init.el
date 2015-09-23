93531046c863a7391eee398833df0e8648cd71d8
Basel Farah <baself@gmail.com>
2015-09-23 10:48:27 +0100

    install magit

--- init.el
+++ init.el
@@ -156,6 +156,7 @@
 ;; =================================
 ;; magit
 ;; =================================
+(install-package-and-require 'magit)
 (global-set-key (kbd "C-c C-g") 'magit-status)
 
 
@@ -316,6 +317,7 @@
 (install-package-and-require 'smartparens)
 (smartparens-global-mode t)
 (sp-use-paredit-bindings)
+(sp-pair "'" nil :actions :rem)
 (add-hook 'lisp-mode-hook 'smartparens-strict-mode)
 (add-hook 'emacs-lisp-mode-hook 'smartparens-strict-mode)
 (add-hook 'scheme-mode-hook 'smartparens-strict-mode)
