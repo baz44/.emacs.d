b17ba0111bf91269a2f015bca27b60d92757ca85
Basel Farah <baself@gmail.com>
2015-09-24 15:37:51 +0100

    added clj-refactor and clojure-cheatcheet

--- init.el
+++ init.el
@@ -373,6 +373,27 @@
 
 
 ;; =================================
+;; clj-refactor
+;; =================================
+(install-package-and-require 'clj-refactor)
+
+(defun my-clojure-mode-hook ()
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1) ; for adding require/use/import
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+
+
+
+;; =================================
+;; clojure-cheatsheet
+;; =================================
+(install-package-and-require 'clojure-cheatsheet)
+
+
+
+;; =================================
 ;; set js indent level
 ;; =================================
 (setq js-indent-level 2)
