bccfc4509e75df0a20e59349e21b52f321c74859
Basel Farah <baself@gmail.com>
2025-01-12 06:16:03 +0000

    Trying out copilot

--- init.el
+++ init.el
@@ -568,6 +568,62 @@
 (use-package ein
     :ensure t)
 
+
+;; =================================
+;; copilot
+;; =================================
+(install-package-and-require 'copilot)
+
+
+;; (defun rk/no-copilot-mode ()
+;;   "Helper for `rk/no-copilot-modes'."
+;;   (copilot-mode -1))
+
+;; (defvar rk/no-copilot-modes '(shell-mode
+;;                               inferior-python-mode
+;;                               eshell-mode
+;;                               term-mode
+;;                               vterm-mode
+;;                               comint-mode
+;;                               compilation-mode
+;;                               debugger-mode
+;;                               dired-mode-hook
+;;                               compilation-mode-hook
+;;                               flutter-mode-hook
+;;                               minibuffer-mode-hook)
+;;   "Modes in which copilot is inconvenient.")
+
+;; (defun rk/copilot-disable-predicate ()
+;;   "When copilot should not automatically show completions."
+;;   (or rk/copilot-manual-mode
+;;       (member major-mode rk/no-copilot-modes)
+;;       (company--active-p)))
+
+;; (add-to-list 'copilot-disable-predicates #'rk/copilot-disable-predicate)
+
+;; (defvar rk/copilot-manual-mode nil
+;;   "When `t' will only show completions when manually triggered, e.g. via M-C-<return>.")
+
+;; (defun rk/copilot-change-activation ()
+;;   "Switch between three activation modes:
+;; - automatic: copilot will automatically overlay completions
+;; - manual: you need to press a key (M-C-<return>) to trigger completions
+;; - off: copilot is completely disabled."
+;;   (interactive)
+;;   (if (and copilot-mode rk/copilot-manual-mode)
+;;       (progn
+;;         (message "deactivating copilot")
+;;         (global-copilot-mode -1)
+;;         (setq rk/copilot-manual-mode nil))
+;;     (if copilot-mode
+;;         (progn
+;;           (message "activating copilot manual mode")
+;;           (setq rk/copilot-manual-mode t))
+;;       (message "activating copilot mode")
+;;       (global-copilot-mode))))
+
+;; (define-key global-map (kbd "M-C-<escape>") #'rk/copilot-change-activation)
+
 ;; ==== Everything else
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -578,7 +634,7 @@
  '(custom-safe-themes
    '("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))
  '(package-selected-packages
-   '(ein yasnippet-snippets vterm undo-tree sql-indent smartparens rust-mode projectile multiple-cursors markdown-mode magit imenu-anywhere ido-vertical-mode idle-highlight-mode golden-ratio go-mode focus flycheck fill-column-indicator expand-region exec-path-from-shell deft company color-theme-sanityinc-solarized clojure-snippets cider ag)))
+   '(quelpa f editorconfig ein yasnippet-snippets vterm undo-tree sql-indent smartparens rust-mode projectile multiple-cursors markdown-mode magit imenu-anywhere ido-vertical-mode idle-highlight-mode golden-ratio go-mode focus flycheck fill-column-indicator expand-region exec-path-from-shell deft company color-theme-sanityinc-solarized clojure-snippets cider ag)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
