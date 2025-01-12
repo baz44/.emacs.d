6efb7783258cec3ad1f428f5e5bcd22cb9b7cc5b
Basel Farah <baself@gmail.com>
2025-01-12 07:13:35 +0000

    Better copilot support for emacs

--- init.el
+++ init.el
@@ -573,56 +573,74 @@
 ;; copilot
 ;; =================================
 (install-package-and-require 'copilot)
+(install-package-and-require 'company-box)
 
+(defun rk/no-copilot-mode ()
+  "Helper for `rk/no-copilot-modes'."
+  (copilot-mode -1))
 
-;; (defun rk/no-copilot-mode ()
-;;   "Helper for `rk/no-copilot-modes'."
-;;   (copilot-mode -1))
+(defvar rk/no-copilot-modes '(shell-mode
+                              inferior-python-mode
+                              eshell-mode
+                              term-mode
+                              vterm-mode
+                              comint-mode
+                              compilation-mode
+                              debugger-mode
+                              dired-mode-hook
+                              compilation-mode-hook
+                              flutter-mode-hook
+                              minibuffer-mode-hook)
+  "Modes in which copilot is inconvenient.")
 
-;; (defvar rk/no-copilot-modes '(shell-mode
-;;                               inferior-python-mode
-;;                               eshell-mode
-;;                               term-mode
-;;                               vterm-mode
-;;                               comint-mode
-;;                               compilation-mode
-;;                               debugger-mode
-;;                               dired-mode-hook
-;;                               compilation-mode-hook
-;;                               flutter-mode-hook
-;;                               minibuffer-mode-hook)
-;;   "Modes in which copilot is inconvenient.")
+(defun rk/copilot-disable-predicate ()
+  "When copilot should not automatically show completions."
+  (or rk/copilot-manual-mode
+      (member major-mode rk/no-copilot-modes)
+      (company--active-p)))
 
-;; (defun rk/copilot-disable-predicate ()
-;;   "When copilot should not automatically show completions."
-;;   (or rk/copilot-manual-mode
-;;       (member major-mode rk/no-copilot-modes)
-;;       (company--active-p)))
+(add-to-list 'copilot-disable-predicates #'rk/copilot-disable-predicate)
 
-;; (add-to-list 'copilot-disable-predicates #'rk/copilot-disable-predicate)
+(defvar rk/copilot-manual-mode nil
+  "When `t' will only show completions when manually triggered, e.g. via M-C-<return>.")
 
-;; (defvar rk/copilot-manual-mode nil
-;;   "When `t' will only show completions when manually triggered, e.g. via M-C-<return>.")
+(defun rk/copilot-change-activation ()
+  "Switch between three activation modes:
+- automatic: copilot will automatically overlay completions
+- manual: you need to press a key (M-C-<return>) to trigger completions
+- off: copilot is completely disabled."
+  (interactive)
+  (if (and copilot-mode rk/copilot-manual-mode)
+      (progn
+        (message "deactivating copilot")
+        (global-copilot-mode -1)
+        (setq rk/copilot-manual-mode nil))
+    (if copilot-mode
+        (progn
+          (message "activating copilot manual mode")
+          (setq rk/copilot-manual-mode t))
+      (message "activating copilot mode")
+      (global-copilot-mode))))
 
-;; (defun rk/copilot-change-activation ()
-;;   "Switch between three activation modes:
-;; - automatic: copilot will automatically overlay completions
-;; - manual: you need to press a key (M-C-<return>) to trigger completions
-;; - off: copilot is completely disabled."
-;;   (interactive)
-;;   (if (and copilot-mode rk/copilot-manual-mode)
-;;       (progn
-;;         (message "deactivating copilot")
-;;         (global-copilot-mode -1)
-;;         (setq rk/copilot-manual-mode nil))
-;;     (if copilot-mode
-;;         (progn
-;;           (message "activating copilot manual mode")
-;;           (setq rk/copilot-manual-mode t))
-;;       (message "activating copilot mode")
-;;       (global-copilot-mode))))
+(define-key global-map (kbd "M-C-<escape>") #'rk/copilot-change-activation)
 
-;; (define-key global-map (kbd "M-C-<escape>") #'rk/copilot-change-activation)
+(defun rk/copilot-complete-or-accept ()
+  "Command that either triggers a completion or accepts one if one
+is available. Useful if you tend to hammer your keys like I do."
+  (interactive)
+  (if (copilot--overlay-visible)
+      (progn
+        (copilot-accept-completion)
+        (open-line 1)
+        (next-line))
+    (copilot-complete)))
+
+(define-key copilot-mode-map (kbd "M-C-<next>") #'copilot-next-completion)
+(define-key copilot-mode-map (kbd "M-C-<prior>") #'copilot-previous-completion)
+(define-key copilot-mode-map (kbd "M-C-<right>") #'copilot-accept-completion-by-word)
+(define-key copilot-mode-map (kbd "M-C-<down>") #'copilot-accept-completion-by-line)
+(define-key global-map (kbd "M-C-<return>") #'rk/copilot-complete-or-accept)
+
 
 ;; ==== Everything else
 (custom-set-variables
@@ -634,7 +652,7 @@
  '(custom-safe-themes
    '("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))
  '(package-selected-packages
-   '(quelpa f editorconfig ein yasnippet-snippets vterm undo-tree sql-indent smartparens rust-mode projectile multiple-cursors markdown-mode magit imenu-anywhere ido-vertical-mode idle-highlight-mode golden-ratio go-mode focus flycheck fill-column-indicator expand-region exec-path-from-shell deft company color-theme-sanityinc-solarized clojure-snippets cider ag)))
+   '(company-box copilot quelpa f editorconfig ein yasnippet-snippets vterm undo-tree sql-indent smartparens rust-mode projectile multiple-cursors markdown-mode magit imenu-anywhere ido-vertical-mode idle-highlight-mode golden-ratio go-mode focus flycheck fill-column-indicator expand-region exec-path-from-shell deft company color-theme-sanityinc-solarized clojure-snippets cider ag)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
